6f163f6ba88a0708c044412150c7aa8b1b485ac0
high-web3 <dreamshaper89@gmail.com>
2026-02-06 17:27:19 +0800

    Add files via upload

--- lib/binance.ts
+++ lib/binance.ts
@@ -19,14 +19,14 @@ export type Kline = {
 export const INTERVALS = {
     '1m': '1m',
     '5m': '5m',
+    '15m': '15m',
     '30m': '30m',
     '1h': '1h',
     '2h': '2h',
     '4h': '4h',
-    '8h': '8h',
-    '12h': '12h',
     '1d': '1d',
     '1w': '1w',
+    '1M': '1M',
 };
 
 export const SUPPORTED_COINS = ['BTC', 'ETH', 'SOL', 'LTC', 'DOGE', 'ZEC', 'BNB', 'DASH', 'XAG'];
